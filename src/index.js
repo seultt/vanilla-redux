73d55db9095d227832affc9376c1ebe662e030d6
seulgi.lee <seulgi.lee@byclip.com>
2023-11-21 15:09:14 +0900

    #1.1 Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0) => {
+  console.log(count);
+  return count;
 };
 
-const handleAdd = () => {
-  console.log("add");
-  count = count + 1;
-  updateText();
-};
-const handleMinus = () => {
-  console.log("minus");
-  count = count - 1;
-  updateText();
-};
+const countStore = createStore(countModifier);
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
+
+// let count = 0;
+// number.innerText = count;
+
+// const updateText = () => {
+//   number.innerText = count;
+// };
+
+// const handleAdd = () => {
+//   console.log("add");
+//   count = count + 1;
+//   updateText();
+// };
+// const handleMinus = () => {
+//   console.log("minus");
+//   count = count - 1;
+//   updateText();
+// };
+
+// add.addEventListener("click", handleAdd);
+// minus.addEventListener("click", handleMinus);
