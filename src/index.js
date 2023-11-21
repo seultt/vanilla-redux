443b27c2b8c323ce3786939a917c906176eb6179
seulgi.lee <seulgi.lee@byclip.com>
2023-11-21 15:25:01 +0900

    #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -4,14 +4,34 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-  console.log(count);
-  return count;
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return (count = count + 1);
+  } else if (action.type === "MINUS") {
+    return (count = count - 1);
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
+number.innerText = countStore.getState();
 
-console.log(countStore.getState());
+const updateText = () => {
+  number.innerText = countStore.getState();
+};
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+  updateText();
+};
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+  updateText();
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
+// console.log(countStore.getState());
 
 // let count = 0;
 // number.innerText = count;
