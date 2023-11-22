455e79fffad4ebc5c99e7deecafe84e9c6287e8b
seultt <seultt04@gmail.com>
2023-11-22 10:33:33 +0900

    2.0 Vanilla ToDo: Pre-Practice

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
+import { createStore } from "redux";
+
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createToDo = (toDo) => {
+const CREATE_TO_DO = "CREATE_TO_DO";
+const toDoModifier = (toDo = [], action) => {
+  console.log(action);
+  switch (action.type) {
+    case CREATE_TO_DO:
+      return [...toDo, action.value];
+
+    default:
+      return toDo;
+  }
+};
+
+const toDoStore = createStore(toDoModifier);
+const createToDo = () => {
   const li = document.createElement("li");
-  li.innerText = toDo;
+  li.innerText = toDoStore.getState();
   ul.appendChild(li);
 };
+toDoStore.subscribe(createToDo);
 
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  createToDo(toDo);
+  toDoStore.dispatch({ type: CREATE_TO_DO, value: toDo });
+  // createToDo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
