4f3e4e115d13ff67f1fb30162c471379cf2b379b
seultt <seultt04@gmail.com>
2023-11-22 11:44:59 +0900

    #2.2 Delete To Do

--- src/index.js
+++ src/index.js
@@ -6,49 +6,59 @@ const ul = document.querySelector("ul");
 
 const ADD_TODO = "ADD_TODO";
 const DELETE_TO_DO = "DELETE_TO_DO";
+
+const addToDo = (value) => {
+  return {
+    type: ADD_TODO,
+    value,
+  };
+};
+
+const deleteToDo = (id) => {
+  return { type: DELETE_TO_DO, id };
+};
+
 const toDoModifier = (toDo = [], action) => {
   switch (action.type) {
     case ADD_TODO:
       return [{ id: new Date().getTime(), text: action.value }, ...toDo];
     case DELETE_TO_DO:
-      return toDo.filter((item) => item.id !== action.id);
+      return toDo.filter((item) => item.id !== parseInt(action.id));
 
     default:
       return toDo;
   }
 };
 
-const toDoStore = createStore(toDoModifier);
+const store = createStore(toDoModifier);
 
-const addToDo = (value) => {
-  toDoStore.dispatch({
-    type: ADD_TODO,
-    value,
-  });
+const dispatchAddToDo = (value) => {
+  store.dispatch(addToDo(value));
 };
-
-const deleteToDo = (id) => {
-  toDoStore.dispatch({ type: DELETE_TO_DO, id });
+const dispatchDeleteToDo = (e) => {
+  const id = e.target.parentNode.id;
+  store.dispatch(deleteToDo(id));
 };
 const paintToDos = () => {
   ul.innerHTML = "";
-  toDoStore.getState().forEach((element) => {
+  store.getState().forEach((element) => {
     const li = document.createElement("li");
     const button = document.createElement("button");
     button.innerHTML = "delete";
-    button.addEventListener("click", () => deleteToDo(element.id));
+    button.addEventListener("click", dispatchDeleteToDo);
+    li.id = element.id;
     li.innerText = element.text;
     li.appendChild(button);
     ul.appendChild(li);
   });
 };
-toDoStore.subscribe(paintToDos);
+store.subscribe(paintToDos);
 
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  addToDo(toDo);
+  dispatchAddToDo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
