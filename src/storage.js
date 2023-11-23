63c564085411a34f4d81cf58f213a4b91eb3ed6d
seultt <seultt04@gmail.com>
2023-11-23 10:53:51 +0900

    getStorage, setStorage trycatch 추가

--- src/storage.js
+++ src/storage.js
@@ -1,8 +1,19 @@
 export const getStorage = () => {
-  let data = localStorage.getItem("data");
-  return data ? JSON.parse(data) : data;
+  try {
+    let data = localStorage.getItem("data");
+    if (data === null) {
+      return undefined;
+    }
+    return data ? JSON.parse(data) : data;
+  } catch (error) {
+    return undefined;
+  }
 };
 export const setStorage = (key, value) => {
-  const stringifyValue = JSON.stringify(value);
-  localStorage.setItem(key, stringifyValue);
+  try {
+    const stringifyValue = JSON.stringify(value);
+    localStorage.setItem(key, stringifyValue);
+  } catch (error) {
+    // error
+  }
 };
